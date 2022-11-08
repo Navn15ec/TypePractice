cba3d5066d90ee0aebc791564a5e9538e3dae0ce
Navn15ec <naveenaradhya5556@gmail.com>
2022-11-08 08:57:37 +0530

    Removed the hello world

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import TodoView from './features/Todo/TodoView';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      
       <CakeView />
       <TodoView />
     </div>
